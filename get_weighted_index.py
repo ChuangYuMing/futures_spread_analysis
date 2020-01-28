297599a8658530df99da71307fabfbe857bac5b7
莊育銘 <chming28@gmail.com>
2020-01-29 00:14:26 +0800

    refactor: change to class base

--- get_weighted_index.py
+++ get_weighted_index.py
@@ -7,145 +7,114 @@ import json
 import time
 import datetime
 import collections
+from random import randint
 from re import sub
 from decimal import Decimal
 import firebase_admin
+from package.tools import check_date, is_settle, format_number
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate('firebase-adminsdk.json')
-default_app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+print('START ############## WeightedIndextCrawler', datetime.datetime.now())
 
-params = {
-  "response": "json",
-  "date": "20160105",
-  "type": "MS"
-}
-count = 0
+class WeightedIndextCrawler:
+    def __init__(self):
+        self.url = 'https://www.twse.com.tw/indicesReport/MI_5MINS_HIST'
+        self.data = collections.OrderedDict()
+        self.params = {
+            "response": "json",
+            "date": "20160105"
+        }
+    def main(self):
+        cred = credentials.Certificate('firebase-adminsdk.json')
+        default_app = firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
 
-td_month = 1
-td_final_price = 5
+        self.request_data()
 
+    def saveDatas(self, year):
+        ref = self.db.collection('weighted_index').document(str(year))
+        ref.set(self.data)
+        self.data = collections.OrderedDict()
 
-def find_tr_index(table):
-    tr_index = 1
-    month = table.select("tr")[tr_index].select("td")[td_month].text
-    while "W" in month:
+    # 西元
+    def format_ad_date(self, date):
+        date_arr = date.split('/')
+        year = date_arr[0]
+        month = date_arr[1]
+        day = date_arr[2]
+        year = str(int(year) + 1911)
+        return year + '/' + month + '/' + day
 
-        tr_index = tr_index + 1
-        month = table.select("tr")[tr_index].select("td")[td_month].text
-    return tr_index
+    def get_last_update_date(self):
+        docs = self.db.collection('weighted_index').stream()
 
+        for lastDoc in docs:
+            pass
 
-def format_year(year):
-    n_year = int(year) + 1911
-    return str(n_year)
+        sortedDoc = sorted(lastDoc.to_dict().items())
+        return sortedDoc[-1][0]
 
+    def request_data(self):
+        last_update_date = self.get_last_update_date()
+        last_update_datetime = datetime.datetime.strptime(last_update_date, "%Y/%m/%d").date()
+        last_year = last_update_datetime.year
+        last_month = last_update_datetime.month
+        
+        now_date = datetime.date.today()
+        now_year = now_date.year
+        now_month = now_date.month
 
-def format_number(num):
-    value = Decimal(sub(r'[^\d.]', '', num))
-    value = value*-1 if "-" in num else value
-    return str(value)
+        while (last_year <= now_year):
+            if last_year < now_year:
+                target_month = 12
+            else :
+                target_month = now_month
 
-# 西元
-def format_ad_date(date):
-    date_arr = date.split('/')
-    year = date_arr[0]
-    month = date_arr[1]
-    day = date_arr[2]
-    year = str(int(year) + 1911)
-    return year + '/' + month + '/' + day
+            while (last_month <= target_month):
+                syear = str(last_year)
+                smonth = str(last_month) if len(str(last_month)) != 1 else "0" + str(last_month)
+                sday = "01"
 
-# 日期是否有意義
-def check_date(year, month, day):
-    now_date = datetime.datetime.now()
-    try:
-        request_date = datetime.date(year, month, day)
-        nowstamp = time.mktime(now_date.timetuple())
-        requeststamp = time.mktime(request_date.timetuple())
-        diff = requeststamp - nowstamp
-        if now_date.day == day:
-            if diff < -72000:
-                return True
-            else:
-                return False
-        else:
-            if diff > 0:
-                return False
-            else:
-                return True
-    except ValueError:
-        return False
-    return True
+                datestart = syear + "/" + smonth + "/" + sday
+                isValidDate = check_date(datestart, "/")
 
+                if isValidDate:
+                    self.params["date"] = syear + smonth + sday
+                    try:
+                        print(self.params["date"])
+                        res = requests.get(self.url, params=self.params)
+                        time.sleep(randint(1, 2))
+                    except requests.exceptions.RequestException as e:
+                        print(e)
+                        time.sleep(120)
+                        print('re connect')
+                        res = requests.get(self.url, params=self.params)
 
-# 是否為結算日
-def is_settle(date):
-    date_arr = date.split('/')
-    year = int(date_arr[0])
-    month = int(date_arr[1]) if date_arr[1][0:1] != "0" else int(date_arr[1][-1])
-    day = int(date_arr[2])
-    n_date = datetime.date(year, month, day)
-    weekday = n_date.strftime("%w")
+                    res = res.json()
+                    if (res['stat']) == 'OK':
+                        datas = res["data"]
+                        for item in datas:
+                            datestart = self.format_ad_date(item[0])
+                            self.data[datestart] = {}
+                            self.data[datestart]["open"] = format_number(item[1].split(".")[0]) # 開盤
+                            self.data[datestart]["high"] = format_number(item[2].split(".")[0]) # 最高
+                            self.data[datestart]["low"] = format_number(item[3].split(".")[0])  # 最低
+                            self.data[datestart]["w_index"] = format_number(item[4].split(".")[0]) # 收盤
+                            self.data[datestart]["is_settle"] = is_settle(datestart, '/')
 
-    if weekday == "3":
-        bb_date = n_date + datetime.timedelta(days=-21)
-        b_date = n_date + datetime.timedelta(days=-14)
-        a_date = n_date + datetime.timedelta(days=7)
-        if b_date.month == month and a_date.month == month and bb_date.month != month:
-            return True
-    return False
+                    else:
+                        print("no data")
 
+                last_month = last_month + 1
 
-def request_data(url, params):
-    global count
-    print(params["date"])
-    res = requests.get(crawl_url, params=params)
-    aa = ''
-    count = count + 1
-    if count > 10:
-        time.sleep(2)
-        count = 0
-    try:
-        aa = json.loads(res.text)
-        if isinstance(aa, dict) is True:
-            return aa
-    except:
-        time.sleep(2)
-        aa = request_data(url, params)
-        if isinstance(aa, dict) is True:
-            return aa
+            self.saveDatas(last_year)
+            last_year = last_year + 1
+            last_month = 1
 
+Crawler = WeightedIndextCrawler()
 
-data = collections.OrderedDict()
-for z in range(2018, 2021):
-    for y in range(1, 13):
-        syear = str(z)
-        smonth = str(y) if len(str(y)) != 1 else "0" + str(y)
-        sday = "01"
-        if check_date(z, y, 1) is not True:
-            continue
-        params["date"] = syear + smonth + sday
-        params["response"] = "json"
-        crawl_url = "https://www.twse.com.tw/indicesReport/MI_5MINS_HIST"
-        res = request_data(crawl_url, params)
+if __name__ == "__main__":
+    Crawler.main()
 
-        if (res['stat']) == 'OK':
-            datas = res["data"]
-            for item in datas:
-                datestart = format_ad_date(item[0])
-                data[datestart] = {}
-                data[datestart]["open"] = format_number(item[1]).split(".")[0] # 開盤
-                data[datestart]["high"] = format_number(item[2]).split(".")[0] # 最高
-                data[datestart]["low"] = format_number(item[3]).split(".")[0]  # 最低
-                data[datestart]["w_index"] = format_number(item[4]).split(".")[0] # 收盤
-                data[datestart]["is_settle"] = is_settle(datestart)
-
-        else:
-            print("no data")
-            print(params["date"])
-
-    ref = db.collection('weighted_index').document(str(z))
-    ref.set(data)
-    data = collections.OrderedDict()
+print('END ##############')
