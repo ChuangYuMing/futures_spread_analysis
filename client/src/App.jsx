d462af230a6d0877675988e21349e75dd4e47c4c
JamieChuang <chming28@gmail.com>
2021-08-22 22:59:24 +0800

    fix 404.html for gh-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory
+} from 'react-router-dom'
 import OptionOpenInterestVIew from './view/OptionOpenInterestVIew'
 import FuturesOpenInterestView from './view/FuturesOpenInterestView'
 import FuturesBigOpenInterestView from './view/FuturesBigOpenInterestView'
@@ -7,7 +12,15 @@ import Navigation from './components/navigation/Navigation'
 import LoanAndLendingAnalysis from './components/loan-and-lending-analysis/LoanAndLendingAnalysis'
 
 function App() {
+  const history = useHistory()
   const pathPrefix = process.env.REACT_APP_PUBLIC_URL
+
+  if (window.location.hash && process.env.REACT_APP_IS_GH_PAGE === 'true') {
+    const path = window.location.hash.replace('#/', '')
+    const pathName = window.location.pathname
+    history.push(`${pathName}${path}`)
+  }
+
   return (
     <div className="App">
       <Router>
