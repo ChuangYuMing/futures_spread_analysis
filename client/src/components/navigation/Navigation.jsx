f6fd3189c4169fd7ad62e45a941d57b845e9aaaa
JamieChuang <chming28@gmail.com>
2021-08-22 21:56:41 +0800

    fix gh-page router

--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -1,46 +1,52 @@
 import React from 'react'
-import { NavLink, useRouteMatch } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import clsx from 'clsx'
 
 function Navigation() {
   const pathPrefix = process.env.REACT_APP_PUBLIC_URL
+  const location = useLocation()
+
   const navMap = [
     {
       text: '選擇權未平倉',
-      route: 'option-open-interest',
-      isMatch: !!useRouteMatch(`${pathPrefix}/option-open-interest`)
+      route: `${pathPrefix}/option-open-interest`
     },
     {
       text: '期貨未平倉',
-      route: 'futures-open-interest',
-      isMatch: !!useRouteMatch(`${pathPrefix}/futures-open-interest`)
+      route: `${pathPrefix}/futures-open-interest`
     },
     {
       text: '期貨大額交易人未沖銷',
-      route: 'futures-big-open-interest',
-      isMatch: !!useRouteMatch(`${pathPrefix}/futures-big-open-interest`)
+      route: `${pathPrefix}/futures-big-open-interest`
     },
     {
       text: '融券(借券賣出)分析',
-      route: 'loan-and-lending-analysis',
-      isMatch: !!useRouteMatch(`${pathPrefix}/loan-and-lending-analysis`)
+      route: `${pathPrefix}/loan-and-lending-analysis`
     }
   ]
 
   return (
     <div className="sticky top-0 z-10 flex items-center justify-start pb-1 mb-5 bg-white border-b border-gray-500">
-      {navMap.map(item => (
-        <NavLink
-          key={item.text}
-          className={clsx(
-            'px-4 py-2 text-lg  hover:text-orange-700',
-            item.isMatch ? 'text-orange-700' : 'text-coolGray-700'
-          )}
-          to={`${pathPrefix}/${item.route}`}
-        >
-          {item.text}
-        </NavLink>
-      ))}
+      {navMap.map(item => {
+        const { route } = item
+        const path = location.pathname
+        const isRoot = path === `${pathPrefix}/`
+        const optionOpenNav = `${pathPrefix}/option-open-interest`
+        return (
+          <NavLink
+            key={item.text}
+            className={clsx(
+              'px-4 py-2 text-lg  hover:text-orange-700',
+              path === route || (isRoot && route === optionOpenNav)
+                ? 'text-orange-700'
+                : 'text-coolGray-700'
+            )}
+            to={`${item.route}`}
+          >
+            {item.text}
+          </NavLink>
+        )
+      })}
     </div>
   )
 }
